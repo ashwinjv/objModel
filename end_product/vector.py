a0831ad9f361c6af55adbf00ecfb6d75411aabc4
Ashwin Venkatesan <ashwinjv@gmail.com>
2017-05-02 03:06:19 -0700

    reorder code in end_product vector to match slides

--- end_product/vector.py
+++ end_product/vector.py
@@ -7,6 +7,18 @@ class Vector:
     def __init__(self, components):
         self.components = tuple(components)
 
+    @property
+    def u(self):
+        return self.components[0]
+
+    @property
+    def v(self):
+        return self.components[1]
+
+    @property
+    def angle(self):
+        return math.atan2(self.v, self.u)
+        
     def __len__(self):
         return len(self.components)
 
@@ -14,7 +26,7 @@ class Vector:
         return str(self.components)
 
     def __repr__(self):
-        return '{} object: {}'.format(self.__class__.__name__, str(self))
+        return '{} object: {}'.format(self.__class__.__name__, self)
 
     def __iter__(self):
         return iter(self.components)
@@ -43,13 +55,12 @@ class Vector:
 
     def __mul__(self, number):
         if not isinstance(number, numbers.Real):
-            print('not a real number')
             raise NotImplemented
         return Vector([number * i for i in self])
 
     def __rmul__(self, number):
         return self * number
-        
+
     def __abs__(self):
         return math.sqrt(sum(i * i for i in self))
 
@@ -59,21 +70,8 @@ class Vector:
     def __getitem__(self, key):
         if isinstance(key, str):
             return getattr(self, key)
-        if isinstance(key, numbers.Integral):
-            return self.components[key]
         if isinstance(key, slice):
             return self.components[key]
-
-    @property
-    def u(self):
-        return self.components[0]
-
-    @property
-    def v(self):
-        return self.components[1]
-
-    @property
-    def angle(self):
-        return math.atan2(self.v, self.u)
-
-    
+        if isinstance(key, numbers.Integral):
+            return self.components[key]
+        raise NotImplemented
