1241fc0690afe2c36f53e0dd398ea86b799acbf7
Ashwin Venkatesan <ashwinjv@gmail.com>
2017-05-02 00:55:51 -0700

    add check for wind_db object being None

--- pyweather.py
+++ pyweather.py
@@ -1,13 +1,15 @@
 from utils import DBConnector
 
 def print_oc_wind_vectors():
+    
     with DBConnector(host = "localhost", 
                      user = "admin", 
                      passwd = "password",
                      db="wind") as wind_db:
-        for vector in wind_db.vectors("OC_WIND_COMPONENTS"):
-            print('u_comp:', vector.u)
-            print('v_comp', vector.v)
-            print('magnitude:', abs(vector))
-            print('angle:', vector.angle)
+        if wind_db:
+            for vector in wind_db.vectors("OC_WIND_COMPONENTS"):
+                print('u_comp:', vector.u)
+                print('v_comp', vector.v)
+                print('magnitude:', abs(vector))
+                print('angle:', vector.angle)
 
